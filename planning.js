4c30e7b39d93f40671d1a714cee409da89e3f331
Hasnat <webdevhasnat@gmail.com>
2023-03-11 22:09:13 +0600

    getting started

---
+++ planning.js
@@ -0,0 +1,6 @@
+// <------- Ecommerce website planning -------->
+// 1. home page
+// 5. about page
+// 3. shop page
+// 2. contact page
+// 4. cart page
--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My name is Abul Hasnat</h1>
+      <h2>My name is Abul Hasnat</h2>
+      <h3>My name is Abul Hasnat</h3>
+      <h4>My name is Abul Hasnat</h4>
+      <p>My name is Abul Hasnat</p>
     </div>
   );
 }
 
 export default App;
+
+// main demo : https://ninetheme.com/themes/venam/v3/contact/
+// demo 1 : https://demo.xpeedstudio.com/marketov2/home5/product/branch-apple/
+// demo 2 : https://demo.xpeedstudio.com/marketov2/furniture/
+// demo 3 : courier section : https://preview.themeforest.net/item/cartzilla-angular-15-multipurpose-ecommerce-template/full_screen_preview/43384083?_ga=2.13760628.237134032.1678507484-1054978182.1676535390
